d107de959bb06c81148b3afa10df4dca1913a20c
Mahesh Swaminathan <maheshswaminathan@Maheshs-MacBook-Air.local>
2024-01-31 20:30:44 -0500

    first commit

---
+++ junk.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+JUNK_DIR=~/.junk
+
+function help {
+  echo "Usage: `basename $0` [-hlp] [list of files]
+   -h: Display help.
+   -l: List junked files.
+   -p: Purge all files.
+   [list of files] with no other arguments to junk those files.";
+}
+
+function create_junk_dir {
+  if [ ! -d $JUNK_DIR ]; then
+    mkdir $JUNK_DIR
+  fi
+}
+
+function list_junk_dir {
+  create_junk_dir;
+  ls -lAF $JUNK_DIR;
+}
+
+function purge_junk_dir {
+  rm -ri $JUNK_DIR/*;
+}
+
+function add_junk_dir {
+  create_junk_dir
+  mv $1 $JUNK_DIR;
+}
+
+if [ $# -eq 0 ]
+then
+  help
+  exit 1
+fi
+
+FLAGS=''
+while getopts ":hlp" opt
+do
+    case $opt in
+        h) FLAGS+='h' ;;
+        l) FLAGS+='l' ;;
+        p) FLAGS+='p' ;;
+        *) echo "Error: Unknown option '-$OPTARG'"; help; exit 1;;
+        ?) help; exit 1;; 
+    esac
+done
+
+if [[ "$FLAGS" = "h" ]]; then
+  help; exit 0;
+elif [[ "$FLAGS" = "l" ]]; then
+  list_junk_dir; exit 0;
+elif [[ "$FLAGS" = "p" ]]; then
+  purge_junk_dir; exit 0;
+elif [[ "$FLAGS" != "" ]]; then
+  echo "Error: Too many options enabled."
+  help; exit 1;
+fi
+
+for arg
+do
+    if [ ! -f $arg ]; then
+        echo "Warning: '$arg' not found"
+    else
+        add_junk_dir $arg
+    fi
+done
